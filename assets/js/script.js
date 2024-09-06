d46cf3f593e505cae48681eb8d88b2f86eef9b31
DARROSCelian <celian.dar@gmail.com>
2024-09-06 12:06:02 +0200

    ajout mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
-function showresponsivemenu() {
-    
+const burger = document.querySelector("#burger")
+const cross = document.querySelector("#cross")
+const header = document.querySelector("header")
+
+burger.addEventListener("click", toggleMenu)
+cross.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+    header.classList.toggle("mobile")
 }
